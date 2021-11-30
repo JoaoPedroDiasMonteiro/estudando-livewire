4d86437e2b99756dbeef00be21b95d58273f19e1
Joao Pedro Dias <joaopedro453@hotmail.com>
2021-11-30 07:55:43 -0300

    feat: adicionado queues

--- app/Listeners/SendWelcomeNotification.php
+++ app/Listeners/SendWelcomeNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendWelcomeNotification
+class SendWelcomeNotification implements ShouldQueue
 {
     public function __construct()
     {
--- app/Notifications/OtherDeviceNotification.php
+++ app/Notifications/OtherDeviceNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class OtherDeviceNotification extends Notification
+class OtherDeviceNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
